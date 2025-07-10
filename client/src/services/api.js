e883cd086f5b46716d8a812cafd49db391fdb957
Ritom <chakrabortyarindam053@gmail.com>
2025-07-10 12:43:05 +0530

    a little change on app.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 // Create axios instance
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 // Debug: Log the API URL being used
-console.log('API Base URL:', process.env.REACT_APP_API_URL || 'http://localhost:5000/api');
+console.log('API Base URL:', process.env.REACT_APP_API_URL);
 console.log('Environment API URL:', process.env.REACT_APP_API_URL);
 
 // Request interceptor to add auth token
